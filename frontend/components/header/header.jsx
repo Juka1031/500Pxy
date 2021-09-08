bcd214b2753a1dad5487c1228b5d5b3b50219efd
julian <juka1031@gmail.com>
2021-09-08 11:36:59 -0400

    heroku push2

--- frontend/components/header/header.jsx
+++ frontend/components/header/header.jsx
@@ -1,7 +1,7 @@
 import React from "react"
 import { Link } from "react-router-dom"
 import LoginFormContainer from "../session_form/login_form_container"
-
+import Logo from 'Images/Logo.PNG'
 
 const Header = ({loggedStatus, logout}) => {
 
@@ -9,20 +9,22 @@ const Header = ({loggedStatus, logout}) => {
     // return (
     // <div>
         
-
+    
     // </div>
     // )
     if (loggedStatus) {//if a user is logged in, present a nav bar with log out
         
         return(
+            
             <div>
-                <a href="#">Pxy</a>
-                <a href="#">Discover &#812;</a>
-                <a href="#">Licensing</a>
-                <a href="#">Portfolio</a>
-                <a href="#">Memberships</a>
-                <a href="#">Quests</a>
-                <a href="#">Blog</a>
+                
+                <span ><a href="#"><img src={Logo} className="Logo"/></a></span>
+                <span><a href="#">Discover</a></span>
+                <span><a href="#">Licensing</a></span>
+                <span><a href="#">Portfolio</a></span>
+                <span><a href="#">Memberships</a></span>
+                <span><a href="#">Quests</a></span>
+                <span><a href="#">Blog</a></span>
                 <a href="#">Profile picture button</a>
                 <a href="#">Airplane</a>
                 <a href="#">Notification</a>
@@ -36,7 +38,7 @@ const Header = ({loggedStatus, logout}) => {
         return(
             <div className="header-container">
                 <div className = "header-left-side">
-                    <span><a href="#">Pxy</a></span>
+                    <span ><a href="#"><img src={Logo} className="Logo"/></a></span>
                     <span><a href="#">Discover</a></span>
                     <span><a href="#">Licensing</a></span>
                     <span><a href="#">Resources</a></span>
--- frontend/components/splash/splash.jsx
+++ frontend/components/splash/splash.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import { Link } from 'react-router-dom'
-// import MyImage from 'Images/1.jpg'
+import Vine from 'Images/2.PNG'
+import Pencil from 'Images/3.PNG'
+import Graph from 'Images/4.PNG'
 // import Icon from ''
 const Splash = () => {
 
@@ -19,14 +21,17 @@ const Splash = () => {
                 <h2>What makes us different</h2>
                 <div className="about-content-container">
                     <div className="about-content">
+                        <img src={Vine}/>
                         <h4>Grow as a photographer</h4>
                         <h5>Get immediate exposure with your first upload. Our Pulse algorithm surfaces new photographs and photographers, ensuring your photos are seen by the community so you receive valuable feedback on day one.</h5>
                     </div>
                     <div className="about-content">
+                        <img src={Pencil}/>
                         <h4>Build your career</h4>
                         <h5>Present yourself as a professional. Display your Services, create a Directory listing and get hired, showcase your articles, presets, videos, and more with Resources, and curate Galleries to highlight your work.</h5>
                     </div>
                     <div className="about-content">
+                        <img src={Graph}/>
                         <h4>See how you're performing</h4>
                         <h5>With Statistics and Pulse you get valuable insights into how your photos are performing and how you rank in comparison to other photographers in the community.</h5>
                     </div>
